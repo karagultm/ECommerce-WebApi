0b6f636e3cd0d151ee13cbd146306d89079e0b8f
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-08-15 13:30:14 +0300

    Temel entity tabloları oluşturuldu.

---
+++ Core/YoutubeApi.Domain/Entities/Brand.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Domain.Common;
+
+namespace YoutubeApi.Domain.Entities
+{
+    public class Brand : EntityBase
+    {
+        //burada IEntityBase yazsan da olur yazmasan da sonuçta entitybase
+        //ientitybaseden kalıtım aldığından dolayı dolaylı olarak oda kalıtım alıyor
+
+        public Brand()
+        {
+
+        }
+        public Brand(string name)
+        {
+            Name = name;
+        }
+        public required string Name { get; set; }
+    }
+}
