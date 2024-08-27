9ae3ef0dc0e701d175f77ed752ffa434f9cc7af8
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-08-27 11:49:14 +0300

    Global exception handling yapıldı.

--- Presentation/YoutubeApi.Api/Program.cs
+++ Presentation/YoutubeApi.Api/Program.cs
@@ -1,6 +1,7 @@
 ﻿using YoutubeApi.Persistence;
 using YoutubeApi.Application;
 using YoutubeApi.Mapper;
+using YoutubeApi.Application.Exceptions;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -33,6 +34,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureExceptionHandlingMiddleware(); // bu şekilde middleware ımı applicationıma run time ıma eklemiş oldum
 
 app.UseAuthorization();
 
