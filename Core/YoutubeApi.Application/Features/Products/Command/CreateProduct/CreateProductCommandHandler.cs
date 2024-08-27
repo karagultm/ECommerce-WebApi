85f72235a36cb4456324659fc4c9554d3c5c7a8d
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-08-27 15:08:51 +0300

    Fluent validation pipeline behaviour yapıldı

--- Core/YoutubeApi.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
+++ Core/YoutubeApi.Application/Features/Products/Command/CreateProduct/CreateProductCommandHandler.cs
@@ -4,7 +4,7 @@ using YoutubeApi.Domain.Entities;
 
 namespace YoutubeApi.Application.Features.Products.Command.CreateProduct
 {
-    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest>
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommandRequest, Unit>
 
     {
         private readonly IUnitOfWork unitOfWork;
@@ -13,7 +13,7 @@ namespace YoutubeApi.Application.Features.Products.Command.CreateProduct
         {
             this.unitOfWork = unitOfWork;
         }
-        public async Task Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
+        public async Task<Unit> Handle(CreateProductCommandRequest request, CancellationToken cancellationToken)
         {
             Product product = new(request.Title, request.Description, request.BrandId, request.Price, request.Discount);
 
@@ -29,6 +29,8 @@ namespace YoutubeApi.Application.Features.Products.Command.CreateProduct
                     });
                 await unitOfWork.SaveAsync();
             }
+
+            return Unit.Value;
 
         }
     }
