f8202baae955c05fd27307be48ff1ef6d2f21585
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-08-28 10:34:41 +0300

    Projeyi identity db ye geçirdik. User ve role entityleri oluşturuldu

--- Infrastructure/YoutubeApi.Persistence/Registration.cs
+++ Infrastructure/YoutubeApi.Persistence/Registration.cs
@@ -3,6 +3,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using YoutubeApi.Application.Interfaces.Repositories;
 using YoutubeApi.Application.Interfaces.UnitOfWorks;
+using YoutubeApi.Domain.Entities;
 using YoutubeApi.Persistence.Context;
 using YoutubeApi.Persistence.Repositories;
 using YoutubeApi.Persistence.UnitOfWorks;
@@ -14,13 +15,25 @@ namespace YoutubeApi.Persistence
         public static void AddPersistence(this IServiceCollection services, IConfiguration configuration)
         {
 
-            services.AddDbContext<AppDbContext>(opt => 
+            services.AddDbContext<AppDbContext>(opt =>
             opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
 
             services.AddScoped(typeof(IReadRepository<>), typeof(ReadRepository<>));
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
 
-            services.AddScoped<IUnitOfWork,UnitOfWork>();
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+
+            services.AddIdentityCore<User>(opt =>
+            {
+                opt.Password.RequireNonAlphanumeric = false;
+                opt.Password.RequiredLength = 2;
+                opt.Password.RequireLowercase = false;
+                opt.Password.RequireUppercase = false;
+                opt.Password.RequireDigit = false;
+                opt.SignIn.RequireConfirmedEmail = false;
+            })
+                .AddRoles<Role>()
+                .AddEntityFrameworkStores<AppDbContext>();
         }
     }
 }
