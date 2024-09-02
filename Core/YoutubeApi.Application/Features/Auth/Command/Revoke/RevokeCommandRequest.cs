eb15f301ee4ebcb9f53b189843dbda87cea105ff
Muhammed Talha Karagul <talha.karagul@is.net.tr>
2024-09-02 17:22:05 +0300

    Logout işlemleri - revoke and revokeall fonksiyonları tamamlandı

---
+++ Core/YoutubeApi.Application/Features/Auth/Command/Revoke/RevokeCommandRequest.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace YoutubeApi.Application.Features.Auth.Command.Revoke
+{
+    public class RevokeCommandRequest : IRequest<Unit>
+    {
+        public string Email { get; set; }
+
+    }
+}
